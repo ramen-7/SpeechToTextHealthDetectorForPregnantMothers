eac844ecea0d688be886cd223180c7f915167a45
Shivam Taneja <76044999+ramen-7@users.noreply.github.com>
2022-06-29 12:09:51 +0530

    uploading the correct file

--- voiceAssistant/model.py
+++ voiceAssistant/model.py
@@ -1,6 +1,27 @@
 import speech_recognition as sr
-
 import pyttsx3 as pt
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+
+
+def split_data(df):
+    X = df.drop(['RiskLevel'], axis=1)
+    y = df.RiskLevel
+    return X, y
+
+
+def preprocess_scaling(x):
+    x = sc.fit_transform(x)
+    return x
+
+
+def scale_predict(x):
+    x = sc.transform(x)
+    return x
+
 
 listener = sr.Recognizer()
 speak = pt.init()
@@ -9,7 +30,7 @@ key_val = {'SystolicBP': 0,
            'Age': 0,
            'BS': 0.0,
            'BodyTemp': 0.0,
-           'HeartRate':0}
+           'HeartRate': 0}
 
 
 def talk(text):
@@ -56,18 +77,58 @@ def get_value(key, value):
 
 
 if __name__ == '__main__':
-    print("Logs")
+    df = pd.read_csv('Maternal Health Risk Data Set.csv')
+    sc = StandardScaler()
+    le = LabelEncoder()
+    X, y = split_data(df)
+    X.BodyTemp = (X.BodyTemp - 32) * 5 / 9  # converting to celcius
+    y = le.fit_transform(y)
+    X = preprocess_scaling(X)
+    xgb_clf = xgb.XGBClassifier(n_estimator=66, learning_rate=0.09, max_depth=9, random_state=7)
+    xgb_clf = xgb_clf.fit(X, y)
+    print("Logs: --------------------------------------------------------------------------------")
     print(("Hi, I will be assisting you with the filling of the data, please speak the values for the variables as I "
            "say them"))
     talk("Hi, I will be assisting you with the filling of the data, please speak the values for the variables as I "
          "say them")
     to_get = {'SystolicBP': 'upper blood pressure or systolic blood pressure',
               'DiastolicBP': 'lower blood pressure or diastolic blood pressure',
-              'Age': 'age',
+              'Age': 'age in years',
               'BS': 'blood glucose level or blood sugar level',
               'BodyTemp': 'body temperature in Celsius',
               'HeartRate': 'heart rate'}
     for key, value in to_get.items():
         get_value(key, value)
+        print("---------------------------------------------------------------------------------")
     talk("Thank you for sharing the patient's data with me")
 
+
+    age = key_val['Age']
+    systolicBp = key_val['SystolicBP']
+    diastolicBp = key_val['DiastolicBP']
+    bloodSugar = key_val['BS']
+    bodyTemp = key_val['BodyTemp']
+    heartRate = key_val['HeartRate']
+    pred = [[age], [systolicBp], [diastolicBp], [bloodSugar], [bodyTemp], [heartRate]]
+    pred = np.array(pred)
+    pred = np.reshape(pred, (1, 6))
+    pred = scale_predict(pred)
+
+    ans = xgb_clf.predict(pred)
+    if ans == [0]:
+        print('Mother is at Low Risk')
+        talk('Mother is at Low Risk')
+        key_val['RiskLevel'] = ans[0]
+    elif ans == [1]:
+        print('Mother is at Medium Risk')
+        talk('Mother is at Medium Risk')
+        key_val['RiskLevel'] = ans[0]
+    else:
+        print('Mother is at High Risk')
+        talk('Mother is at High Risk')
+        key_val['RiskLevel'] = ans[0]
+
+    df.append(key_val, ignore_index=True)
+    df = df.to_csv('Maternal Health Risk Data Set.csv', index=False)
+    talk("Thank you!")
+    print("---------------------------------------------------------------------------------")
